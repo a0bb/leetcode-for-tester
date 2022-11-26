1bed3387c9bfe17d49f045adfed54043b042206c
wangshihua <wangsh.a0bb@gmail.com>
2022-11-26 22:42:47 +0800

    feat:移除元素

---
+++ src/移除元素.py
@@ -0,0 +1,32 @@
+# -*-coding:utf-8 -*-
+# Author     ：wangdahua
+# Email      ：wangsh.a0bb@gmail.com
+# Time       ：2022/11/26 22:19
+from typing import List
+
+
+# 力扣地址
+# https://leetcode.cn/problems/remove-element/
+
+# 使用快慢指针解决此问题，快指针来遍历数组，慢指针用来收集不等于 val 的元素
+class Solution:
+    def remove_element(self, nums: List[int], val: int) -> int:
+        """
+        移除目标元素
+        :param nums:
+        :param val:
+        :return: 移除后数组的新长度
+        """
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != val:
+                nums[slow] = nums[fast]
+                slow += 1
+        return slow
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.remove_element([1, 2, 3, 4, 5, 2], 2))
+    print(s.remove_element([1, 2, 3, 4, 5, 2], -1))
+    print(s.remove_element([], -1))
